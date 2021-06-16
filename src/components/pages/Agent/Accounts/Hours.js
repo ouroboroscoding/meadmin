7d05d4e4ffb16e37f53e70fdad92cbe4d1458e43
Chris Nasr <chris@ouroboroscoding.com>
2021-06-16 15:08:15 -0400

    - Rebuilt Day/Time components since I had only handled complete hours before

--- src/components/pages/Agent/Accounts/Hours.js
+++ src/components/pages/Agent/Accounts/Hours.js
@@ -15,8 +15,6 @@ import React, { useEffect, useState } from 'react';
 // Material UI
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
-import FormControl from '@material-ui/core/FormControl';
-import InputLabel from '@material-ui/core/InputLabel';
 import Select from '@material-ui/core/Select';
 import Switch from '@material-ui/core/Switch';
 import Typography from '@material-ui/core/Typography';
@@ -30,16 +28,6 @@ import { clone, omap } from 'shared/generic/tools';
 
 // Constants
 const DAYS = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
-const HOURS = [
-	['12:00 AM EST', '00:00:00'], [' 1:00 AM EST', '01:00:00'], [' 2:00 AM EST', '02:00:00'],
-	[' 3:00 AM EST', '03:00:00'], [' 4:00 AM EST', '04:00:00'], [' 5:00 AM EST', '05:00:00'],
-	[' 6:00 AM EST', '06:00:00'], [' 7:00 AM EST', '07:00:00'], [' 8:00 AM EST', '08:00:00'],
-	[' 9:00 AM EST', '09:00:00'], ['10:00 AM EST', '10:00:00'], ['11:00 AM EST', '11:00:00'],
-	['12:00 PM EST', '12:00:00'], [' 1:00 PM EST', '13:00:00'], [' 2:00 PM EST', '14:00:00'],
-	[' 3:00 PM EST', '15:00:00'], [' 4:00 PM EST', '16:00:00'], [' 5:00 PM EST', '17:00:00'],
-	[' 6:00 PM EST', '18:00:00'], [' 7:00 PM EST', '19:00:00'], [' 8:00 PM EST', '20:00:00'],
-	[' 9:00 PM EST', '21:00:00'], ['10:00 PM EST', '22:00:00'], ['11:00 PM EST', '23:00:00']
-];
 const NICE_DAYS = {
 	sun: 'Sunday',
 	mon: 'Monday',
@@ -48,6 +36,180 @@ const NICE_DAYS = {
 	thu: 'Thursday',
 	fri: 'Friday',
 	sat: 'Saturday'
+}
+
+/**
+ * Time
+ *
+ * Handles a single time (hours, minutes)
+ *
+ * @name Time
+ * @access private
+ * @param Object props Attributes sent to the component
+ * @return React.Component
+ */
+function Time(props) {
+
+	// State
+	let [state, stateSet] = useState({
+		hour: 0,
+		minutes: 0,
+		period: 'AM'
+	});
+
+	// Effect when the value is changed
+	useEffect(() => {
+
+		// Split into 3 parts and convert to ints
+		let lTime = props.value.split(':')
+
+		// Init the new state
+		let oState = {
+			hour: parseInt(lTime[0], 10),
+			minutes: lTime[1],
+			period: 'AM'
+		}
+
+		// If the hour is more than 12
+		if(lTime[0] > 12) {
+			oState.hour -= 12;
+			oState.period = 'PM';
+		}
+
+		// Set the state
+		stateSet(oState);
+
+	}, [props.value]);
+
+	// Calculates and returns the actual ISO time
+	function calculate(state) {
+
+		// Init the parts
+		let lTime = ['', state.minutes, '00']
+
+		// If we're in PM
+		if(state.period === 'PM') {
+			lTime[0] = (state.hour + 12).toString();
+		}
+
+		// Else, if the time is less then 10
+		else if(state.hour < 10) {
+			lTime[0] = '0' + state.hour;
+		}
+
+		// Else, store as is
+		else {
+			lTime[0] = state.hour.toString()
+		}
+
+		// Join and return
+		return lTime.join(':');
+	}
+
+	// Called when the hour changes
+	function hour(ev) {
+
+		// Change the state
+		stateSet(state => {
+
+			// Change the hour
+			state.hour = parseInt(ev.target.value);
+
+			// Calculate the iso and send it to the parent
+			props.onChange(calculate(state));
+
+			// Clone and return
+			return clone(state);
+		});
+	}
+
+	// Called when the minute changes
+	function minutes(ev) {
+
+		// Change the state
+		stateSet(state => {
+
+			// Change the minutes
+			state.minutes = ev.target.value;
+
+			// Calculate the iso and send it to the parent
+			props.onChange(calculate(state));
+
+			// Clone and return
+			return clone(state);
+		});
+	}
+
+	// Called when the period changes
+	function period(ev) {
+
+		// Change the state
+		stateSet(state => {
+
+			// Change the pm
+			state.period = ev.target.value;
+
+			// Calculate the iso and send it to the parent
+			props.onChange(calculate(state));
+
+			// Clone and return
+			return clone(state);
+		});
+	}
+
+	// Render
+	return (
+		<React.Fragment>
+			<Select
+				native
+				onChange={hour}
+				value={state.hour.toString()}
+			>
+				<option>12</option>
+				<option>1</option>
+				<option>2</option>
+				<option>3</option>
+				<option>4</option>
+				<option>5</option>
+				<option>6</option>
+				<option>7</option>
+				<option>8</option>
+				<option>9</option>
+				<option>10</option>
+				<option>11</option>
+			</Select> : <Select
+				native
+				onChange={minutes}
+				value={(state.minutes < 10 ? '0' : '') + state.minutes.toString()}
+			>
+				<option>00</option>
+				<option>05</option>
+				<option>10</option>
+				<option>15</option>
+				<option>20</option>
+				<option>25</option>
+				<option>30</option>
+				<option>35</option>
+				<option>40</option>
+				<option>45</option>
+				<option>50</option>
+				<option>55</option>
+			</Select> <Select
+				native
+				onChange={period}
+				value={state.period}
+			>
+				<option>AM</option>
+				<option>PM</option>
+			</Select>
+		</React.Fragment>
+	);
+}
+
+// Force props
+Time.propTypes = {
+	onChange: PropTypes.func.isRequired,
+	value: PropTypes.string.isRequired
 }
 
 /**
@@ -77,13 +239,13 @@ function Day(props) {
 	}
 
 	// Called to update the start
-	function startUpdate(ev) {
+	function startUpdate(time) {
 
 		// Clone the existing value
 		let oValue = clone(props.value);
 
 		// Set the new start
-		oValue.start = ev.target.value;
+		oValue.start = time;
 
 		// If the new value is further than the end
 		if(oValue.start > oValue.end) {
@@ -95,13 +257,13 @@ function Day(props) {
 	}
 
 	// Called to update the end
-	function endUpdate(ev) {
+	function endUpdate(time) {
 
 		// Clone the existing value
 		let oValue = clone(props.value);
 
 		// Set the new end
-		oValue.end = ev.target.value;
+		oValue.end = time;
 
 		// If the new value is before the start
 		if(oValue.end < oValue.start) {
@@ -111,6 +273,8 @@ function Day(props) {
 		// Let the parent know
 		props.onChange(props.dow, oValue);
 	}
+
+	console.log(props);
 
 	// Render
 	return (
@@ -126,33 +290,15 @@ function Day(props) {
 			<Box className="flexGrow">
 				{props.value &&
 					<React.Fragment>
-						<FormControl size="small" variant="outlined">
-							<InputLabel htmlFor={'hoursStart-' + props.dow} style={{backgroundColor: 'white', padding: '0 5px'}}>Start</InputLabel>
-							<Select
-								inputProps={{id: 'hoursStart-' + props.dow}}
-								native
-								onChange={startUpdate}
-								value={props.value.start}
-							>
-								{HOURS.map(l =>
-									<option key={l[1]} value={l[1]}>{l[0]}</option>
-								)}
-							</Select>
-						</FormControl>
-						&nbsp;
-						<FormControl size="small" variant="outlined">
-							<InputLabel htmlFor={'hoursEnd-' + props.dow} style={{backgroundColor: 'white', padding: '0 5px'}}>End</InputLabel>
-							<Select
-								inputProps={{id: 'hoursEnd-' + props.dow}}
-								native
-								onChange={endUpdate}
-								value={props.value.end}
-							>
-								{HOURS.map(l =>
-									<option key={l[1]} value={l[1]}>{l[0]}</option>
-								)}
-							</Select>
-						</FormControl>
+						<Time
+							onChange={startUpdate}
+							value={props.value.start}
+						/>
+						&nbsp;&nbsp;&nbsp;to&nbsp;&nbsp;&nbsp;
+						<Time
+							onChange={endUpdate}
+							value={props.value.end}
+						/>
 					</React.Fragment>
 				}
 			</Box>
